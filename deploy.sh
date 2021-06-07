8c3c3fcc57f8f596984a3162da6032e7fa274794
Arkadiusz Kołodziejek <arkko002@gmail.com>
2021-06-07 15:33:20 +0200

    Add deploy npm script

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,14 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-echo 'https://arkko002.github.io/arkko/' > CNAME
+# echo 'https://arkko002.github.io/arkko/' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
- git push -f git@github.com:arkko002/arkko002.github.io.git main
+ git push -f git@github.com:arkko002/arkko.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
