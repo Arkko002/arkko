9154c2d2059a007092ceaae3aa4c4fe3178a2e32
Arkadiusz Kołodziejek <arkko002@gmail.com>
2021-06-07 14:21:40 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'https://arkko002.github.io/arkko/' > CNAME
 
 git init
 git add -A
--- vue.config.ts
+++ vue.config.ts
@@ -1,2 +1,3 @@
 module.exports = {
+	publicPath: process.env.NODE_ENV === "production" ? "/arkko/" : "/",
 };
