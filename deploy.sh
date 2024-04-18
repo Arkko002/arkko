60c376b3572235e44555c45a8f98efc77068990c
Arkko <arkko002@gmail.com>
2024-04-18 13:24:50 +0200

    Use new migration branch for deployment

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
- git push -f git@github.com:arkko002/arkko.git master:gh-pages
+git push -f git@github.com:arkko002/arkko.git vue3-migration:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
